38330a601ead3cc365d55c50c8c2feb578d1ada7
Julien Combattelli <julien.combattelli@hotmail.com>
2017-02-09 17:46:43 +0100

    Add demo code for motor driving

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include "Menu/Menu.h"
 
 /*
  * Tx1 PA9 Rx1 PA10
@@ -12,9 +13,6 @@
  */
 
 DigitalOut myled(LED1);
-
-Serial bt(PA_9, PA_10, 115200);
-Serial pc(USBTX, USBRX);
 PwmOut mot1(PA_8);
 PwmOut mot2(PA_11);
 AnalogIn adc1(PA_0);
@@ -23,16 +21,49 @@ AnalogOut dac1(PA_6);
 AnalogOut dac2(PA_5);
 I2C i2c(PB_7,PB_6);
 
+#define STEP (0.05f)
+
+void mot1p_handler()
+{
+	printf("mot1 : %d%%\r\n", int(mot1.read()*100));
+	mot1 = mot1 + STEP;
+}
+
+void mot1m_handler()
+{
+	printf("mot1 : %d%%\r\n", int(mot1.read()*100));
+	mot1 = mot1 - STEP;
+}
+
+void mot2p_handler()
+{
+	printf("mot2 : %d%%\r\n", int(mot2.read()*100));
+	mot2 = mot2 + STEP;
+}
+
+void mot2m_handler()
+{
+	printf("mot2 : %d%%\r\n", int(mot2.read()*100));
+	mot2 = mot2 - STEP;
+}
+
 int main()
 {
-	pc.puts("Test start\r\n");
-	bt.puts("hello\r\n");
+	mot1 = 1;
+	mot2 = 1;
 
-    while(1)
-    {
-        myled = 1; // LED is ON
-        wait(1); // 200 ms
-        myled = 0; // LED is OFF
-        wait(1); // 1 sec
-    }
+	Menu_init("Beautifull menu");
+	Menu_add_item("Mot1 +", mot1p_handler);
+	Menu_add_item("Mot1 -", mot1m_handler);
+	Menu_add_item("Mot2 +", mot2p_handler);
+	Menu_add_item("Mot2 -", mot2m_handler);
+
+	while(1)
+	{
+		puts("\r\n");
+		Menu_print();
+		char userInput = 0;
+		userInput = getchar();
+		Menu_choose(userInput-'0');
+	}
 }
